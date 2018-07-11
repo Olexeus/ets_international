c83c099e023ac34c7391f24715e1092fc230b8cf
Olexeus <fedorenkoalex23@gmail.com>
2018-07-11 11:42:25 +0300

    Commit 7 Complete main page without animation

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function (){
-	// scrollto id
+
+	// Scroll-to-id
 	$(".scroll-to-id").on('click', 'a[href^="#"]', function(e) {
 		// target element id
 		var id = $(this).attr('href');
@@ -12,9 +13,7 @@ $(document).ready(function (){
 			return;
 		} else {
 			var $id = $(id);
-			if ($id.length === 0) {
-					return;
-			}
+			if ($id.length === 0) { return; }
 			// top position relative to the document
 			var pos = $id.offset().top - $(".mnu").outerHeight(true);
 			// animated top scrolling
@@ -24,10 +23,8 @@ $(document).ready(function (){
 		
 	});
 
-	// Declare Carousel jquery object
+	// Main slider
 	var owl = $('.owl-one');
-
-	// Carousel initialization
 	owl.owlCarousel({
 		loop:true,
 		margin:0,
@@ -45,58 +42,43 @@ $(document).ready(function (){
 		dragEndSpeed: 500
 	});
 
-
-	// add animate.css class(es) to the elements to be animated
-	function setAnimation ( _elem, _InOut ) {
-		// Store all animationend event name in a string.
-		// cf animate.css documentation
-		var animationEndEvent = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-
-		_elem.each ( function () {
-			var $elem = $(this);
-			var $animationType = 'animated ' + $elem.data( 'animation-' + _InOut );
-
-			$elem.addClass($animationType).one(animationEndEvent, function () {
-				$elem.removeClass($animationType); // remove animate.css Class at the end of the animations
-			});
-		});
-	}
-
-// Fired before current slide change
-owl.on('change.owl.carousel', function(event) {
-	var $currentItem = $('.owl-item', owl).eq(event.item.index);
-	var $elemsToanim = $currentItem.find("[data-animation-out]");
-	setAnimation ($elemsToanim, 'out');
-});
-
-// Fired after current slide has been changed
-var round = 0;
-owl.on('changed.owl.carousel', function(event) {
-
-	var $currentItem = $('.owl-item', owl).eq(event.item.index);
-	var $elemsToanim = $currentItem.find("[data-animation-in]");
-	
-	setAnimation ($elemsToanim, 'in');
-})
-
-/*  owl.on('translated.owl.carousel', function(event) {
-		console.log (event.item.index, event.page.count);
-		
-			if (event.item.index == (event.page.count - 1))  {
-				if (round < 1) {
-					round++
-					console.log (round);
-				} else {
-					owl.trigger('stop.owl.autoplay');
-					var owlData = owl.data('owl.carousel');
-					owlData.settings.autoplay = false; //don't know if both are necessary
-					owlData.options.autoplay = false;
-					owl.trigger('refresh.owl.carousel');
-				}
+	// Clients slider
+	var owl = $('.owl-two');
+	owl.owlCarousel({
+		loop:true,
+		margin:0,
+		navSpeed:500,
+		nav:true,
+		navText : ['<img src="../img/clients/arr-prev.png" alt="" class="arr-prev">','<img src="../img/clients/arr-next.png" alt="" class="arr-next">'],
+		autoplay: true,
+		autoplayHoverPause: true,
+		rewind: false,
+		smartSpeed: 500,
+		autoplaySpeed: 500,
+		navSpeed: 500,
+		dotsSpeed: 500,
+		dragEndSpeed: 500,
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1,
+				nav:true,
+				slideBy:1
+			},
+			600:{
+				items:3,
+				nav:false,
+				slideBy:3
+			},
+			1000:{
+				items:5,
+				nav:true,
+				slideBy: 5
 			}
-		});*/
+		}
+	});
 
-	// Button animation
+	// Menu button animation
 	$(".sandwich, .mnu-item").click(function() {
 		if ($(".mnu-line, .mnu-item, .mnu-mob").hasClass("active") && $('.sandwich').is(':visible')) {
 			$(".mnu-line, .mnu-item, .mnu-mob").fadeOut(600).toggleClass("active");
@@ -107,40 +89,42 @@ owl.on('changed.owl.carousel', function(event) {
 		}
 	});
 
+	// Solutions animation
 	$(".solutions-item-btn-cont, .arr-up>a").on('click', function (e) {
 		var href = $(this).attr('href');
-		var hidden = false;
+		var expand = "#solution-0";
 		for (var i = $(".solutions-expand").length; i >= 0; i--) {
 			expand = "#solution-"+i;
-			if ($(expand).hasClass("active") && expand == href) { // close if the only open
-				$(href).slideUp( "slow", function() {
-					$(this).removeClass("active");
-					var pos = $("#solutions").offset().top - $(".mnu").outerHeight(true);
-					$('body, html').animate({scrollTop: pos});
-				});
-				hidden = true;
-				console.log(href + " " + 1 + " i = " + i);
-			} else if (i == 0 && !$(href).hasClass("active") && hidden == false) { // add if there are no open
-				$(href).slideDown( "slow", function() {
-					$(this).addClass("active");
-				});
-				console.log(href + " " + 2 + " i = " + i);
-			} // !$("#solution-3").hasClass("active")
-			else if ($(expand).hasClass("active") && !$(href).hasClass("active")) {
-				$(href).toggleClass("active");
-				$(expand).removeClass("active");
-				$(expand).slideUp("slow", function () {
-					$(href).slideToggle( "slow");
-					var pos = $(href).offset().top - $(".mnu").outerHeight(true);
-					$('body, html').animate({scrollTop: pos});
-				});
-				console.log(href + " " + 3 + " i = " + i);
-			} else { // just close
-				$(expand).removeClass("active");
-				$(expand).slideUp("slow");
-				console.log(href + " " + 4);
-			}			
+			if ($(expand).hasClass("active")) { 
+				expand = expand;
+				break;
+			} else {
+				expand = "#solution-0";
+			}       
 		}
-		// href = href.replace('#solution-', '');
+		if (expand != "#solution-0" && expand == href) { // close if the only open
+			$(href).slideUp( "slow", function() {
+				$(this).removeClass("active");
+			});
+			console.log(href + " 1");
+		} else if (expand == "#solution-0") { // add if there are no open
+			$(href).slideDown( "slow", function() {
+				$(this).addClass("active");
+			});
+			console.log(href + " 2");
+		} else if (expand != "#solution-0" && expand != href) {
+			$(href).toggleClass("active");
+			$(expand).removeClass("active");
+			$(expand).slideUp("slow", function () {
+				$(href).slideToggle("slow");
+				var pos = $(href).offset().top - $(".mnu").outerHeight(true);
+				$('body, html').animate({scrollTop: pos});
+			});
+			console.log(href + " 3");
+		} else { // just close
+			$(expand).removeClass("active");
+			$(expand).slideUp("slow");
+			console.log(href + " 4");
+		}
 	});
 });
